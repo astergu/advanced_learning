e280ca324a234d80fff8e1c62cc8f54a7df63ad5
Peiqin <gupeiqin@baidu.com>
2016-08-16 20:55:36 +0800

    update

--- practice/lexicon.h
+++ practice/lexicon.h
@@ -5,12 +5,20 @@ namespace com {
 namespace baidu {
 namespace utility {
 
-    struct LexiconTable {
-        int col_num;
-    };
+struct LexiconTable {
+    int col_num;
+};
 
-    void parse_from_file(std::string path);
-    void parse_from_file(std::ifstream f);
+void parse_from_file(std::string path);
+void parse_from_file(std::ifstream f);
+
+class Singleton {
+    public:
+        static Singleton getInstance();
+    private:
+        static final Singleton instance = new Singleton();
+        Singleton() {}
+};
 }
 }
 }
